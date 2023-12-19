ade342e0305bfe08c63d95267cc7edb4b420e621
David Hansson <david.hansson@vntrs.com>
2023-12-19 19:17:23 +0100

    Add formatter to the extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,48 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const replacements = [
+  { pattern: /^(\s{0,})"/g, replacement: ''},
+  { pattern: /"(\s{0,})$/g, replacement: ''},
+  { pattern: /\\n/g, replacement: '\n'},
+  { pattern: /\\"/g, replacement: '"'},
+  { pattern: /\sfrom|\sFROM/g, replacement: '\nFROM'},
+  { pattern: /\s{0,}select|\s{0,}SELECT/g, replacement: '\nSELECT\n'},
+  { pattern: /\sleft\sjoin|\sLEFT\sJOIN/g, replacement: '\nLEFT JOIN'},
+  { pattern: /\sorder\sby\s|\sORDER\sBY\s/g, replacement: '\n\nORDER BY\n'},
+  { pattern: /\sor|\sOR/g, replacement: '\n\tOR'},
+  { pattern: /\sand|\sAND/g, replacement: '\n\tAND'},
+  { pattern: /\sunion\s|\sUNION\s/g, replacement: '\n\nUNION\n'},
+  { pattern: /,/g, replacement: ',\n'},
+];
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "prettify-postgresql" is now active!');
+	const disposable = vscode.commands.registerCommand('prettify-postgresql.prettify', () => {
+      // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('prettify-postgresql.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Prettify PostgreSQL!');
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const { document, selection } = editor;
+
+      const { start: { line: startLine }, end: { line: endLine } } = selection;
+
+      const selectedLines = Array
+        .from(Array(endLine - startLine + 1)
+          .keys()
+        )
+        .map(n => startLine + n)
+        .map(l => document.lineAt(l).text);
+
+      let sqlString = selectedLines.join('');
+      replacements.forEach((replacement) => {
+        sqlString = sqlString.replace(replacement.pattern, replacement.replacement);
+      });
+
+      editor.edit(editBuilder => editBuilder.replace(selection, sqlString));
+		}
 	});
 
 	context.subscriptions.push(disposable);
